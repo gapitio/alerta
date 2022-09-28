9ba34002bb27be703af89aaf29f341741767bfbd
Sondre Boldvik <sb@gapit.io>
2022-09-28 17:22:54 +0200

    add postgres support for link mobility sms

--- alerta/database/backends/postgres/base.py
+++ alerta/database/backends/postgres/base.py
@@ -90,8 +90,13 @@ class Backend(Database):
             conn = self.connect()
 
             with app.open_resource('sql/schema.sql') as f:
-                conn.cursor().execute(f.read())
-                conn.commit()
+                try:
+                    conn.cursor().execute(f.read())
+                    conn.commit()
+                except Exception as e:
+                    if raise_on_error:
+                        raise
+                    app.logger.warning(e)
 
         register_adapter(dict, Json)
         register_adapter(datetime, self._adapt_datetime)
@@ -993,6 +998,7 @@ class Backend(Database):
             WHERE id=%(id)s
             RETURNING *
         """
+        print(update)
         kwargs['id'] = id
         kwargs['user'] = kwargs.get('user')
         return self._updateone(update, kwargs, returning=True)
@@ -2163,7 +2169,7 @@ class Backend(Database):
                  WHERE (severity=%(inform_severity)s
                         AND last_receive_time < (NOW() at time zone 'utc' - INTERVAL '%(info_threshold)s seconds'))
             """
-            self._deleteall(delete, {'info_threshold': info_threshold})
+            self._deleteall(delete, {'inform_severity': alarm_model.DEFAULT_INFORM_SEVERITY, 'info_threshold': info_threshold})
 
         # get list of alerts to be newly expired
         select = """
